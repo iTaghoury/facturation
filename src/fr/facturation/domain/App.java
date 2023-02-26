2450a3aa4845cfad90ba4dc07fadb874df7ee020
iTaghoury <idriss.taghoury@gmail.com>
2023-02-26 16:15:56 +0100

    Added product list creation method

--- src/fr/facturation/domain/App.java
+++ src/fr/facturation/domain/App.java
@@ -7,6 +7,19 @@ public class App {
 	public static void creerFacture(Client[] clients) {
 		for(Client client : clients) {
 			Facture facture = new Facture(client);
+			creerListeProduits(facture);
+		}
+	}
+	
+	public static void creerListeProduits(Facture facture) {
+		Produit[] produits = {
+				new Produit(2, 4.50f, "Steak haché", Categorie.VIANDE),
+				new Produit(1, 12f, "Lessive", Categorie.MENAGER),
+				new Produit(1, 2.30f, "Penne 500g", Categorie.PATES)
+		};
+		
+		for(Produit produit : produits) {
+			facture.getListeProduits().add(produit);
 		}
 	}
 	
